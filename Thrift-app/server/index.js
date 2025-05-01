022c18184f8284fc320162f67f32e1a190fdf2ec
SAmWaM42 <samuelwamae42@gmail.com>
2025-05-01 14:04:39 +0300

    added a basic button item and form react component

--- Thrift-app/server/index.js
+++ Thrift-app/server/index.js
@@ -6,10 +6,16 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+
 app.get('/api', (req, res) => {
     res.json({ message: 'Hello from the backend!' });
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.get('/home', (req, res) => {
+    res.json({ message: 'This is the home-page!' });
 });
+
